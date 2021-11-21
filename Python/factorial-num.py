3aa116d54267e002e68cf88e34e54da581ad353e
ishakgairaa36 <ishakgairaa36.@gmail.com>
2021-11-21 22:59:44 +0200

    change file of class

--- Python/factorial-num.py
+++
@@ -1,6 +0,0 @@
-# factorial of num
-x =abs(int(input("Enter the number = ")))
-a=1
-for i in range(0,x):
-  a=a*(x-i)
-print("the factorial of",x,"! is >>>",a)
--- Python/sum-number.py
+++
@@ -1,8 +0,0 @@
-# sum of number
-a=a1 =int(input("Please enter the number do you want to sum = "))
-f=0
-while(a>0):
-  b = a % 10
-  f=f+b
-  a=int(a//10)
-print("The sum of",a1,"is>>>",f)
